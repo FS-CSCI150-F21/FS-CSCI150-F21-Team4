6d737c2ec7f9f954f69308a3be0893bae593e56b
Jacob Sales <jacobsales30@gmail.com>
2021-09-29 19:24:04 -0700

    Added home route for pythong template

--- python/app.py
+++ python/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World!"
+	return "Hello World!" 
 
-if __name__ == "main":
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
