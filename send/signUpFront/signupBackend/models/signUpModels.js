2f032c92145492d038e7c796bc1d571fb793ceba
Travis Quigg <62739843+tquigg96@users.noreply.github.com>
2021-10-10 15:12:26 -0700

    Update signUpModels.js

--- send/signUpFront/signupBackend/models/signUpModels.js
+++ send/signUpFront/signupBackend/models/signUpModels.js
@@ -5,38 +5,45 @@ const signUpTemplate = new mongoose.Schema({
 
     firstName:{
         type: String,
+        required: true
         
     },
 
     lastName:{
         type: String,
+        required: true
         
     },
 
     email:{
         type: String,
+        required: true
         
     },
 
     phone:{
         type: String,
+        required: true
         
     },
 
     Labor:{
         type: String,
+        required: true
         
 
     },
 
     userName:{
             type: String,
+            required:true
             
 
     },
 
     password: {
             type: String,
+            required: true
             
     },
   
