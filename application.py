1e9b5917fea908913a810d32aef2d8ab4fc0ddb0
JohnEaganFS <johnjeagan@mail.fresnostate.edu>
2021-10-17 16:15:11 -0700

    Fixing requests

--- application.py
+++ application.py
@@ -10,29 +10,28 @@ client = MongoClient(url)
 
 @application.route("/login", methods = ["POST", "GET"])
 def login():
-	if request.method == "POST":
-		user = request.form['uname']
-		psw = request.form['psw']
-		return redirect(url_for("user", usr = user))
-	else:
-		return render_template("login.html")
+	#if request.method == "POST":
+		#user = request.form['uname']
+		#psw = request.form['psw']
+		#return redirect(url_for("user", usr = user))
+	#else:
+	return render_template("login.html")
 
 @application.route('/register', methods = ['POST', 'GET'])
 def register():
-  if request.method == 'POST':
-    usersDB = client["userRegistration"]
-    users = usersDB['userregistrations']
-    existing_user = users.find_one({'name': request.form['username']})
+    if request.method == 'POST':
+        usersDB = client["userRegistration"]
+        users = usersDB['userregistrations']
+        existing_user = users.find_one({'name': request.form['username']})
 
-    if existing_user is None:
-      hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
-      users.insert_one({'date': datetime.date,'name': request.form['username'], 'password':hashpass}, 'email': request.form['email'], 'phonenumber': request.form['phonenumber'], 'labor':request.form['keywords'])
-      session['username'] = request.form['username']
-      return redirect(url_for('index'))
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            users.insert_one({'date': datetime.date,'name': request.form['username'], 'password':hashpass}, 'email': request.form['email'], 'phonenumber': request.form['phonenumber'], 'labor':request.form['keywords'])
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
 
-    return 'Username already exists'
-  return render_template('register.html')
-  return ''
+        return 'Username already exists'
+    return render_template('register.html')
 
 @application.route("/results")
 def resultsPage():
