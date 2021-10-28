a7d94666bbf621d2361e9c8c97ea5da5deb52923
AnthonyMarin <90010906+AnthonyMarin@users.noreply.github.com>
2021-10-27 22:36:30 -0700

    created test data

--- testData.py
+++ testData.py
@@ -1,37 +1,32 @@
 rslts = [
         {
-			"title": "Thor: Ragnorok",
-			"director": "Taika Waititi",
-			"year": 2017,
-			"rating": 3.5,
+			"userName": "Thor: Ragnorok",
+			"labor": "Taika Waititi",
+			"location": 2017,
 			"imgLink": "https://a.ltrbxd.com/resized/film-poster/2/0/9/3/8/2/209382-thor-ragnarok-0-460-0-690-crop.jpg?k=02c656f39b"
 		},
 		{
-			"title": "My Dinner with Andre",
-			"director": "Louis Malle",
-			"year": 1981,
-			"rating": 5,
+			"userName": "My Dinner with Andre",
+			"labor": "Louis Malle",
+			"location": 1981,
 			"imgLink": "https://a.ltrbxd.com/resized/film-poster/3/6/0/4/2/36042-my-dinner-with-andre-0-460-0-690-crop.jpg?k=c1d3b48517"
 		},
 		{
-			"title": "Moonrise Kingdom",
-			"director": "Wes Anderson",
-			"year": 2012,
-			"rating": 4,
+			"userName": "Moonrise Kingdom",
+			"labor": "Wes Anderson",
+			"location": 2012,
 			"imgLink": "https://a.ltrbxd.com/resized/sm/upload/v5/7f/mn/y8/ysTohGF7NY9Z6MYz9vLNYiyzh0r-0-460-0-690-crop.jpg?k=9bee8528b0"
 		},
 		{
-			"title": "Good Time",
+			"userName": "Good Time",
 			"director": "Josh Safdie, Benny Safdie",
-			"year": 2017,
-			"rating": 4,
+			"location": 2017,
 			"imgLink": "https://a.ltrbxd.com/resized/film-poster/3/6/2/3/6/2/362362-good-time-0-460-0-690-crop.jpg?k=961512abc0"
 		},
 		{
-			"title": "Kill Bill: Vol. 1",
+			"userName": "Kill Bill: Vol. 1",
 			"director": "Quentin Tarantion",
-			"year": 2003,
-			"rating": 5,
+			"location": 2003,
 			"imgLink": "https://a.ltrbxd.com/resized/sm/upload/sw/w2/ep/v4/9O50TVszkz0dcP5g6Ej33UhR7vw-0-460-0-690-crop.jpg?k=604246a795"
 		}
 
