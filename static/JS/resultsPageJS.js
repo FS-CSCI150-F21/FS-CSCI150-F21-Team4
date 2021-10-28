5055594f69272bcd2b15be182a9a3a3a9d3004fd
AnthonyMarin <90010906+AnthonyMarin@users.noreply.github.com>
2021-10-27 21:13:47 -0700

    created JS doc for resultsPage

---
+++ static/JS/resultsPageJS.js
@@ -0,0 +1,7 @@
+
+					function setSearchTerm(){
+						let temp = document.getElementById("searchBar").value;
+						sessionStorage.setItem("searchItem",temp )
+						location.reload();
+						return;
+                    ]
