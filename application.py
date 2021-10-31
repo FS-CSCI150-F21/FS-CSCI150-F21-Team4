a2261e9d21320728bb7c4e50969a26b33749bdb8
Mitchell Agcaoili Maltezo <81717745+maltezoa@users.noreply.github.com>
2021-10-31 15:47:34 -0700

    edits to login.html, edits to application.py
    Application.py is bugged

--- application.py
+++ application.py
@@ -8,20 +8,6 @@ application = Flask(__name__)
 url = 'mongodb+srv://Admin:1234@wordofmouth.yoff3.mongodb.net/userRegistration?retryWrites=true&w=majority'
 client = MongoClient(url)
 
-
-
-@application.route("/login/", methods = ["POST", "GET"])
-def login():
-    usersDB = client["userRegistration"]
-    users = usersDB['userregistrations']
-    login_user = users.find_one({'name': request.form['username']})
-    user_pass = login_user['password']
-
-    if login_user and bcrypt.checkpw(request.form['password'], user_pass):
-        print("Test successful")
-    else:
-        print("Test Failed")
-
 @application.route('/register/', methods = ['POST', 'GET'])
 def register():
     if request.method == 'POST':
@@ -37,6 +23,37 @@ def register():
 
         return 'Username already exists'
     return render_template('register.html')
+
+@application.route("/login/", methods = ["POST", "GET"])
+def login():
+    usersDB = client["userRegistration"]
+    users = usersDB['userregistrations']
+    # Problem exists here, method 1 = 'method' object is not scriptable
+    login_user = users.find_one({'name': request.form.get('username')})
+
+    #if login_user is None:
+    #    return redirect(url_for('register'))
+    # method 2, The browser (or proxy) sent a request that this server could not understand. KeyError: 'username'
+    #login_user = users.find_one({'name': request.form['username']})
+
+    user_pass = login_user['password']
+    login_pass = request.form.get('password')
+
+
+    # Method 1
+    if bcrypt.checkpw(login_pass.encode('utf-8'), user_pass):
+       return 'Test successful'
+    else:
+        return 'Test Failed'
+
+    # Method 2
+    
+    """if login_user:
+        if bcrypt.hashpw(request.form.get('password').encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+            return redirect(url_for(landingPage))
+        return 'Invalid'
+    return 'Invalid'"""
+
 
 @application.route("/results/")
 def resultsPage():
