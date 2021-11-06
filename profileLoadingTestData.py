29dbeef337c8b3582188cad05946ab5e05d32bf0
AnthonyMarin <90010906+AnthonyMarin@users.noreply.github.com>
2021-11-06 11:42:24 -0700

    created fake profile data

---
+++ profileLoadingTestData.py
@@ -0,0 +1,35 @@
+     profileResult={
+		 "profile": {
+			"userName": "Joe Swanson",
+			"labor": "Gardener",
+			"location": "Fresno",
+			"imgLink": "https://a.ltrbxd.com/resized/film-poster/2/0/9/3/8/2/209382-thor-ragnarok-0-460-0-690-crop.jpg?k=02c656f39b",
+		 },
+		 "projects" : [
+		{
+			"projectName" : "Estate Garden",
+			"projectDescription": " gabba gabbe",
+			"photoLink1" : "..\static\Images\testData Images\projectImg1.jpg",
+			"photoLink2" : "..\static\Images\testData Images\projectImg2.jpg",
+			"photoLink3" : "..\static\Images\testData Images\projectImg3.jpg",
+			"photoLink4" : "..\static\Images\testData Images\projectImg4.jpg"
+		},
+		{
+			"projectName" : "Estate Garden",
+			"projectDescription": " gabba gabbe",
+			"photoLink1" : "..\static\Images\testData Images\projectImg1.jpg",
+			"photoLink2" : "..\static\Images\testData Images\projectImg2.jpg",
+			"photoLink3" : "..\static\Images\testData Images\projectImg3.jpg",
+			"photoLink4" : "..\static\Images\testData Images\projectImg4.jpg"
+		},
+		{
+			"projectName" : "Estate Garden",
+			"projectDescription": " gabba gabbe",
+			"photoLink1" : "..\static\Images\testData Images\projectImg1.jpg",
+			"photoLink2" : "..\static\Images\testData Images\projectImg2.jpg",
+			"photoLink3" : "..\static\Images\testData Images\projectImg3.jpg",
+			"photoLink4" : "..\static\Images\testData Images\projectImg4.jpg"
+		}
+		]
+			
+		    }
