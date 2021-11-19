3dae13ed638b1c25d126c32dfdbcb2f9a6c501e8
Jacob Sales <jacobsales30@gmail.com>
2021-11-19 14:50:56 -0800

    Commit to repo to change workstations. Modified landing page with method to find oauth verifier

--- application.py
+++ application.py
@@ -9,6 +9,9 @@ from testData import rslts
 
 import oauth2 as oauth
 import urllib.parse
+import base64
+import requests
+from twitterScrapeV1 import getValid
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -21,7 +24,11 @@ application = Flask(__name__)
 url = 'mongodb+srv://Admin:1234@wordofmouth.yoff3.mongodb.net/userRegistration?retryWrites=true&w=majority'
 client = MongoClient(url)
 
-twitterAppToken = {}
+twitterAppToken = {
+    "oauth_token" : "",
+    "oauth_token_secret" : "",
+    "oauth_verifier" : ""
+}
 
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
@@ -96,7 +103,70 @@ def landingPage():
             print("No oauth verifier")
         else:
             print("Found oauh verifier!")
-        access_token_url = 'https://api.twitter.com/oauth/access_token'
+            consumer_key = "uaC0BoALYt6SzxdNiWoOpvGym"
+            consumer_secret = "NHc7uMZLM3zk77fZ7EuOzqSfuDnpqN52ZsTiLGh2k5CylsEPpj"
+            access_token_url = 'https://api.twitter.com/oauth/access_token'
+            consumer = oauth.Consumer(consumer_key, consumer_secret)
+            client = oauth.Client(consumer)
+
+            token = oauth.Token(twitterAppToken['oauth_token'], twitterAppToken['oauth_token_secret'])
+            token.set_verifier(oauth_verifier)
+            client = oauth.Client(consumer, token)
+
+            resp, content = client.request(access_token_url, "POST")
+            access_token = dict(urllib.parse.parse_qsl(content.decode("utf-8")))
+
+            dashFound = False
+            newToken = ""
+            currentToken = access_token['oauth_token']
+            for i in range(len(currentToken)):
+                curChar = currentToken[i]
+                if curChar == '-':
+                    dashFound = True
+                elif dashFound is True:
+                    newToken = newToken + curChar
+            print(f"Formatted token is: {newToken}")
+            access_token['oauth_token'] = newToken
+
+            print ("Access Token:")
+            print ("    - oauth_token        = {}".format(access_token['oauth_token']))
+            print ("    - oauth_token_secret = {}".format(access_token['oauth_token_secret']))
+
+            print ("You may now access protected resources using the access tokens above.")
+            consumer_key = access_token['oauth_token']
+            consumer_secret = access_token['oauth_token_secret']
+
+            #------------------- secret key generation----------------
+            key_secret = f'{consumer_key}:{consumer_secret}'.encode('ascii')
+
+            b64_encoded_key = base64.b64encode(key_secret)
+            b64_encoded_key = b64_encoded_key.decode('ascii')
+            base_url = 'https://api.twitter.com/'
+            auth_url = '{}oauth2/token'.format(base_url)
+
+            auth_headers = {
+                'Authorization': 'Basic {}'.format(b64_encoded_key),
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+            }
+
+            auth_data = {
+                'grant_type': 'client_credentials'
+            }
+
+            auth_resp = requests.post(auth_url, headers=auth_headers, data=auth_data)
+            if getValid(auth_resp) == False:
+                return [], False
+            access_token = auth_resp.json()['access_token']
+
+            #------------------- Start of Mention Finder--------------
+            ##Create HTTP headers
+            search_headers = {
+                'Authorization': 'Bearer {}'.format(access_token)    
+            }
+            response = requests.get(url = 'https://api.twitter.com/1.1/account/verify_credentials.json' , headers=search_headers)
+            print(response.status_code)
+            credVerify = response.json()
+            print(credVerify)
     return render_template("landingPage.html")
 
 @application.route("/<usr>/")
@@ -117,8 +187,8 @@ def NLP():
     consumer_key = "uaC0BoALYt6SzxdNiWoOpvGym"
     consumer_secret = "NHc7uMZLM3zk77fZ7EuOzqSfuDnpqN52ZsTiLGh2k5CylsEPpj"
 
-    request_token_url = 'https://api.twitter.com/oauth/request_token'
-    authorize_url = 'https://api.twitter.com/oauth/authorize'
+    callbackURL = 'http://127.0.0.1:5000/'
+    request_token_url = f'https://api.twitter.com/oauth/request_token?oauth_callback={callbackURL}'
     authenticate_url = 'https://api.twitter.com/oauth/authenticate'
 
     consumer = oauth.Consumer(consumer_key, consumer_secret)
@@ -130,9 +200,15 @@ def NLP():
 
     request_token = dict(urllib.parse.parse_qsl(content.decode("utf-8")))
 
+    twitterAppToken['oauth_token'] = request_token['oauth_token']
+    twitterAppToken['oauth_token_secret'] = request_token['oauth_token_secret']
+
     print ("Request Token:")
     print ("    - oauth_token        = {}".format(request_token['oauth_token']))
     print ("    - oauth_token_secret = {}".format(request_token['oauth_token_secret'])) 
+
+
+
     redirectURL = "{0}?oauth_token={1}".format(authenticate_url, request_token['oauth_token'])
     return flask.redirect(redirectURL, code=302)    
 
